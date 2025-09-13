6616302763452745688ed6a238ab0deb5b31b8e7
LiquidIce64 <gilyazov.rm2005@gmail.com>
2025-09-14 00:22:44 +0300

    Fixed chat widget toggle logic

--- widgets/chat/chat_widget/chat_widget.py
+++ widgets/chat/chat_widget/chat_widget.py
@@ -15,39 +15,38 @@ class ChatWidget(DraggableWidget, Ui_chat_widget):
         self.icon.setIcon(Icons.TextChat, override_color=True)
         self.btn_settings.setIcon(Icons.Settings)
 
-        self.btn.setCheckable(True)
-
         self.chat = chat
         self.update_chat_info()
         self.chat.changed.connect(self.update_chat_info)
         server = ConfigBackend.session.selected_server
-        selected = self.chat.uuid == server.selected_chat_uuid
-        if selected:
-            self.btn.setChecked(True)
-            server.selected_chat_changed.connect(self.btn.toggle)
+        if self.chat.uuid == server.selected_chat_uuid:
+            server.selected_chat_changed.connect(self.on_deselect)
         self.update_highlight()
 
         self.btn.focused.connect(self.update_highlight)
         self.btn.focusLost.connect(self.update_highlight)
         self.btn.hovered.connect(self.update_highlight)
         self.btn.hoverEnd.connect(self.update_highlight)
-        self.btn.toggled.connect(self.on_toggle)
+        self.btn.clicked.connect(self.on_click)
 
     def update_chat_info(self):
         self.label.setText(self.chat.name)
 
-    def on_toggle(self):
+    def on_click(self):
         server = ConfigBackend.session.selected_server
-        if self.btn.isChecked():
-            server.selected_chat_uuid = self.chat.uuid
-            server.selected_chat_changed.connect(self.btn.toggle)
-        else:
-            server.selected_chat_changed.disconnect(self.btn.toggle)
+        server.selected_chat_uuid = self.chat.uuid
+        server.selected_chat_changed.connect(self.on_deselect)
+        self.update_highlight()
+
+    def on_deselect(self):
+        ConfigBackend.session.selected_server.selected_chat_changed.disconnect(self.on_deselect)
         self.update_highlight()
 
     def update_highlight(self):
-        selected = self.btn.isChecked()
+        selected = (self.chat.uuid == ConfigBackend.session.selected_server.selected_chat_uuid)
         highlight = self.btn.hasFocus() or self.btn.underMouse()
+        self.btn.setProperty('checked', selected)
+        self.btn.style().polish(self.btn)
         self.label.setProperty('highlight', selected or highlight)
         self.label.style().polish(self.label)
         self.icon.setProperty('highlight', selected)
