836648e8cf2bdae86a8d34e92195a2a9718b889a
LiquidIce64 <gilyazov.rm2005@gmail.com>
2025-09-17 22:17:26 +0300

    Added single instance check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QLockFile
 
 from backend import StorageBackend
 from database import Database
@@ -10,6 +11,11 @@ if __name__ == "__main__":
     app.setApplicationName('OpenComms')
 
     StorageBackend.init(test_mode='--test-appdata' in sys.argv)
+    lockfile = QLockFile(StorageBackend.locate_appdata('.lock', allow_empty=False))
+    if not lockfile.tryLock(0):
+        pid = lockfile.getLockInfo()[0]
+        print(f'Lockfile in use by process {pid}')
+        exit(1)
     Database.init(StorageBackend.locate_appdata('app_database.db', allow_empty=False))
 
     window = MainWindow()
