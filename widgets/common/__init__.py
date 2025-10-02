4197980bb37974790fad303a40104436f0992f22
LiquidIce64 <gilyazov.rm2005@gmail.com>
2025-10-02 03:17:01 +0300

    Fixed inconsistent anchoring on chat scroll area

--- widgets/common/__init__.py
+++ widgets/common/__init__.py
@@ -3,5 +3,5 @@ from .frame_button import FrameButton
 from .icon_widget import IconWidget, IconButton
 from .scrollable_frame import ScrollableFrame
 from .draggable_widget import DraggableWidget
-from .custom_scrollbar import CustomScrollBar
+from .custom_scrollbar import CustomScrollBar, AnchoredScrollBar
 from .message_textbox import MessageTextbox
